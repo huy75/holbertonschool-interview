44b5b5476584805b36d9c7607806e8fd399a23c2
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-07-23 09:13:52 +0000

    fix add document

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
 
 def canUnlockAll(boxes):
     """ a method that determines if all the boxes can be opened """
