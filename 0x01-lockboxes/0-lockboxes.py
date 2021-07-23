22c9ff38ef09e862f0fe6a57f6b340b0e1b2abf1
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-07-23 09:22:40 +0000

    fix add extra test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@ You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 """
 
+
 def canUnlockAll(boxes):
     """ a method that determines if all the boxes can be opened """
     if not boxes:
@@ -13,10 +14,12 @@ def canUnlockAll(boxes):
     index = 0
 
     for box in boxes:
+        if len(box) == 0 or index == 0:
+            checker[index] = -1 
         for key in box:
             if key < size and key != index:
                 checker[key] = key
-        if len(checker) == size - 1:
+        if len(checker) == size:
             return True
         index += 1
     return False
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -10,3 +10,6 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
