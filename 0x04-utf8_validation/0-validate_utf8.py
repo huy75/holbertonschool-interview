8c0b3390c007f91a7ad6a605da979348db6963a3
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-11 12:58:29 +0000

    fix bit manipulation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,32 +23,34 @@ def validUTF8(data):
     """
     # Number of bytes in the current UTF-8 character
     n_bytes = 0
-    # For each integer in the data array.
-    for each in data:
-        # Get the binary representation of the least significant 8 bits
-        # for any given number.
-        bin_rep = format(each, '#010b')[-8:]
 
-        # If this is the case then start processing a new UTF-8 character.
+    # Mask to check if the most significant bit is set or not
+    mask1 = 1 << 7
+
+    # Mask to check if the second most significant bit is set or not
+    mask2 = 1 << 6
+    for num in data:
+
+        # Get the number of set most significant bits in the byte if
+        # this is the starting byte of an UTF-8 character.
+        mask = 1 << 7
         if n_bytes == 0:
-            # Get the number of 1s in the beginning of the string.
-            for bit in bin_rep:
-                if bit == '0':
-                    break
+            while mask & num:
                 n_bytes += 1
+                mask = mask >> 1
+
             # 1 byte characters
             if n_bytes == 0:
                 continue
+
             # Invalid scenarios according to the rules of the problem.
             if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            # Else, process integers which are a part of
-            # a UTF-8 character. The pattern is `10xxxxxx`.
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+            # If this byte is a part of an existing UTF-8 character, then we
+            # simply have to look at the two most significant bits and we make
+            # use of the masks we defined before.
+            if not (num & mask1 and not (num & mask2)):
                 return False
-        # We reduce the number of bytes to process by 1 after each integer.
         n_bytes -= 1
-    # This is for the case where we might not have the complete data for
-    # a particular UTF-8 character.
     return n_bytes == 0
